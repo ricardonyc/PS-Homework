4ea5770dd3ae105ddcbdb44637a6f64acc1a5bc1
ricardonyc <rvardo98@gmail.com>
2021-12-09 14:42:54 -0500

    finished bonus question

--- ps-practice/methods/filter.js
+++ ps-practice/methods/filter.js
@@ -16,3 +16,20 @@ const six = (array) => array.filter((x) => x % 6 === 0)
 console.log(six([42, 55, 27, 144, 180, 4, 258, 99, 117, 156]))
 
 // this is a new message to see if the git pull works
+
+
+//Bonus
+//using the .filter(), filter out the Full Stack residents
+
+let users = [
+    { firstName: "Bradley", lastName: "Bouley", role: "Full Stack Resident" },
+    { firstName: "Chloe", lastName: "Alnaji", role: "Full Stack Resident" },
+    { firstName: "Jonathan", lastName: "Baughn", role: "Enterprise Instructor" },
+    { firstName: "Michael", lastName: "Herman", role: "Lead Instructor" },
+    { firstName: "Robert", lastName: "Hajek", role: "Full Stack Resident" },
+    { firstName: "Wes", lastName: "Reid", role: "Instructor" },
+    { firstName: "Zach", lastName: "Klabunde", role: "Instructor" },
+  ];
+  
+  let filteredArray = users.filter((x) => x.role !== "Full Stack Resident");
+  console.log(filteredArray)
